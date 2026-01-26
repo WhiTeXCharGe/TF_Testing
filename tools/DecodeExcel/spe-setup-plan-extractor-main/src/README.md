346f1bfebda6c94dcc953f107eee19aeb688cfed
WhiTe <93659566+WhiTeXCharGe@users.noreply.github.com>
2026-01-26 16:14:10 +0900

    Delete tools/DecodeExcel directory

--- tools/DecodeExcel/spe-setup-plan-extractor-main/src/README.md
+++
@@ -1,5 +0,0 @@
-# README
-
-## main01.py
-
-SPE人員計画Excelファイルから，Bar情報を抜き出し，yamlファイルへ変換するスクリプト．
--- tools/DecodeExcel/spe-setup-plan-extractor-main/src/lib_bar_extractor/__init__.py
+++
@@ -1,2 +0,0 @@
-from .util import process_excel_files  # noqa
-from .bar_extractor import BarExtractorConfig  # noqa
